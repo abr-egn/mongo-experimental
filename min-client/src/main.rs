1d932b7b3e500ea2ae6876cc696f31de5d834721
Abraham Egnor <abraham.egnor@mongodb.com>
2021-06-15 11:12:51 -0400

    1.1.1 + sync

--- min-client/src/main.rs
+++ min-client/src/main.rs
@@ -5,14 +5,24 @@ use tokio_111 as tokio;
 #[cfg(feature = "v111-async-std")]
 use async_std_111 as async_std;
 
-use mongodb::{Client, options::ClientOptions};
-
+#[cfg(not(feature = "sync"))]
 #[cfg_attr(feature = "v111-tokio", tokio::main)]
 #[cfg_attr(feature = "v111-async-std", async_std::main)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
-    let client = Client::with_options(client_options)?;
+    use mongodb::Client;
+
+    let client = Client::with_uri_str("mongodb://localhost:27017").await?;
     let db_count = client.list_databases(None, None).await.unwrap().len();
+    println!("database count: {}", db_count);
+    Ok(())
+}
+
+#[cfg(feature = "sync")]
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use mongodb::sync::Client;
+
+    let client = Client::with_uri_str("mongodb://localhost:27017")?;
+    let db_count = client.list_databases(None, None).unwrap().len();
     println!("database count: {}", db_count);
     Ok(())
 }
