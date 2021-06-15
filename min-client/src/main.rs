4e705fc53e9a122e634f468b8f3990899da3187d
Abraham Egnor <abraham.egnor@mongodb.com>
2021-06-15 11:05:08 -0400

    1.1.1 + async-std

--- min-client/src/main.rs
+++ min-client/src/main.rs
@@ -2,20 +2,17 @@
 use mongodb_111 as mongodb;
 #[cfg(feature = "v111-tokio")]
 use tokio_111 as tokio;
+#[cfg(feature = "v111-async-std")]
+use async_std_111 as async_std;
 
 use mongodb::{Client, options::ClientOptions};
 
-#[cfg(feature = "v111-tokio")]
-#[tokio::main]
+#[cfg_attr(feature = "v111-tokio", tokio::main)]
+#[cfg_attr(feature = "v111-async-std", async_std::main)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
     let client = Client::with_options(client_options)?;
     let db_count = client.list_databases(None, None).await.unwrap().len();
     println!("database count: {}", db_count);
-/* 
-    for db_name in client.list_database_names(None, None).await? {
-        println!("{}", db_name);
-    }
-*/
     Ok(())
 }
