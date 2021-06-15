32510df79d584065b5a984a66ab0b2bcc94144b4
Abraham Egnor <abraham.egnor@mongodb.com>
2021-06-15 10:53:59 -0400

    feature matrix

--- min-client/src/main.rs
+++ min-client/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#[cfg(feature = "v111")]
+use mongodb_111 as mongodb;
+#[cfg(feature = "v111-tokio")]
+use tokio_111 as tokio;
+
+use mongodb::{Client, options::ClientOptions};
+
+#[cfg(feature = "v111-tokio")]
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+    let client = Client::with_options(client_options)?;
+    let db_count = client.list_databases(None, None).await.unwrap().len();
+    println!("database count: {}", db_count);
+/* 
+    for db_name in client.list_database_names(None, None).await? {
+        println!("{}", db_name);
+    }
+*/
+    Ok(())
 }
